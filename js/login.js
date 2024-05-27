9e05a34392f2499f53201ddde576d370612cefa6
christyuda <chrisyuda@ulbi.ac.id>
2024-05-28 00:21:01 +0700

    cek user admin

--- js/login.js
+++ js/login.js
@@ -1,5 +1,70 @@
 import { endpointLogin } from "../js/url.js";
 
+// document
+//   .getElementById("signInButton")
+//   .addEventListener("click", function (event) {
+//     event.preventDefault();
+
+//     const emailOrUsername = document.getElementById("username").value;
+//     const password = document.getElementById("password").value;
+
+//     const data = {
+//       username: emailOrUsername,
+//       password: password,
+//     };
+
+//     fetch(endpointLogin, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(data),
+//     })
+//       .then((response) => {
+//         if (!response.ok) {
+//           if (response.status === 401) {
+//             throw new Error("Password salah. Silakan coba lagi.");
+//           } else {
+//             throw new Error(
+//               "Terjadi kesalahan pada server. Silakan coba lagi nanti."
+//             );
+//           }
+//         }
+//         return response.json();
+//       })
+//       .then((data) => {
+//         console.log("Success:", data);
+//         if (data.token) {
+//           localStorage.setItem("LOGIN", data.token);
+//           document.cookie = `LOGIN=${data.token};path=/;max-age=3600`;
+//           Swal.fire({
+//             icon: "success",
+//             title: "Login Berhasil",
+//             text: "Anda akan diarahkan ke halaman dashboard",
+//             confirmButtonText: "OK",
+//           }).then((result) => {
+//             if (result.isConfirmed) {
+//               window.location.href = "dashboard.html";
+//             }
+//           });
+//         } else {
+//           Swal.fire({
+//             icon: "error",
+//             title: "Oops...",
+//             text: "Token tidak diterima",
+//           });
+//         }
+//       })
+//       .catch((error) => {
+//         console.error("Error:", error);
+//         Swal.fire({
+//           icon: "error",
+//           title: "Error Login",
+//           text: error.message,
+//         });
+//       });
+//   });
+
 document
   .getElementById("signInButton")
   .addEventListener("click", function (event) {
@@ -33,26 +98,19 @@ document
         return response.json();
       })
       .then((data) => {
-        console.log("Success:", data);
         if (data.token) {
           localStorage.setItem("LOGIN", data.token);
           document.cookie = `LOGIN=${data.token};path=/;max-age=3600`;
-          Swal.fire({
-            icon: "success",
-            title: "Login Berhasil",
-            text: "Anda akan diarahkan ke halaman dashboard",
-            confirmButtonText: "OK",
-          }).then((result) => {
-            if (result.isConfirmed) {
-              window.location.href = "dashboard.html";
-            }
-          });
+          return getUserDetails(data.token); // Get user details with the token
         } else {
-          Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Token tidak diterima",
-          });
+          throw new Error("Token tidak diterima");
+        }
+      })
+      .then((user) => {
+        if (user.id_role === 1) {
+          window.location.href = "dashboard-admin.html";
+        } else if (user.id_role === 2) {
+          window.location.href = "dashboard.html";
         }
       })
       .catch((error) => {
@@ -64,3 +122,29 @@ document
         });
       });
   });
+
+function getUserDetails(token) {
+  return fetch("http://127.0.0.1:3000/getme", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      LOGIN: ` ${token}`,
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to fetch user data");
+      }
+      return response.json();
+    })
+    .then((data) => data.user)
+    .catch((error) => {
+      console.error("Error fetching user data:", error);
+      Swal.fire({
+        icon: "error",
+        title: "Failed to fetch user data",
+        text: error.message,
+      });
+      throw error;
+    });
+}
