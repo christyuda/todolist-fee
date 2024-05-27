468bbe9421c1efc6948550e0c0e38bda3e54fd9e
christyuda <chrisyuda@ulbi.ac.id>
2024-05-27 23:59:52 +0700

    feat : update delete function

--- js/url.js
+++ js/url.js
@@ -1,4 +1,5 @@
 export const endpointGetTasks = "http://127.0.0.1:3000/tasks";
+export const endpointGetIdTask = "http://127.0.0.1:3000/task";
 export const endpointLogin = "http://127.0.0.1:3000/login";
 export const endpointTambahData = "http://127.0.0.1:3000/task/insert";
 export const endpointGetProfile = "http://127.0.0.1:3000/getme";
